37eff988e5fdeb04f8b49f6d0cf1be9df08adddc
Sven Mark Hofstra <svenmark10@gmail.com>
2019-06-23 13:04:50 +0200

    Change capital text

--- components/ShoppingList/ShoppingListCard.vue
+++ components/ShoppingList/ShoppingListCard.vue
@@ -6,8 +6,8 @@
     </v-card-text>
     <v-card-actions>
       <v-btn @click="goToList">Go to list</v-btn>
-      <v-btn @click="makeTemplate" color="info">Template</v-btn>
-      <v-btn @click="destroy" color="error">Delete List</v-btn>
+      <v-btn @click="makeTemplate" color="info">To template</v-btn>
+      <v-btn @click="destroy" color="error">Delete list</v-btn>
     </v-card-actions>
   </v-card>
 </template>
