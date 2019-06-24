d57ce4bce5787a5176b1926c37afbc0999f2e56d
Sven Mark Hofstra <s.m.hofstra@swbase.com>
2019-06-24 11:10:20 +0200

    Change header color

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description },
-      { name: 'theme-color', content: '#e17b00'}
+      { name: 'theme-color', content: '#ab5e00'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
